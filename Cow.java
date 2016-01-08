f286958d90e3769c254e43db970f4e9ae70f4709
aahiremath <anup.a.hiremath@gmail.com>
2016-01-08 11:22:58 -0800

    added lmao comment

--- Cow.java
+++ Cow.java
@@ -1,3 +1,11 @@
 public class Cow extends FarmObject{
-	
+	private String name;
+	private int hungriness;
+	private int age;
+	private int sicknessLevel;
+	public void doStuffForAnHour(int hour){
+		if(hour<=18&&hour>=6){
+			//lmao
+		}
+	}
 }
