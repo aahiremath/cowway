5a0154876bb6f8e1d92936fadea297d90e1814ff
jk97 <bleach94539@gmail.com>
2016-01-11 11:15:49 -0800

    fail

--- Cow.java
+++ Cow.java
@@ -1,11 +1,22 @@
-public class Cow extends FarmObject{
+]public class Cow extends FarmObject{
 	private String name;
 	private int hungriness;
 	private int age;
 	private int sicknessLevel;
+	
 	public void doStuffForAnHour(int hour){
 		if(hour<=18&&hour>=6){
-			//lmao
+			Random rand = new Random();			//movie in random direction
+			int direction = rand.nextInt(4);
+			if( direction == 0){		
+				x ++;
+			}else if (direction == 1){
+				x--;
+			} else if (direction == 2){
+				y++;
+			} else {
+				y--;
+			}
 		}
 	}
 }
