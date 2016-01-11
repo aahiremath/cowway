6f2ca67d346766d198464f221d8ada5f816048d2
aahiremath <anup.a.hiremath@gmail.com>
2016-01-11 11:09:48 -0800

    added cow behavior

--- Cow.java
+++ Cow.java
@@ -1,11 +1,30 @@
 public class Cow extends FarmObject{
+	private Random rand = new Random();
 	private String name;
 	private int hungriness;
 	private int age;
 	private int sicknessLevel;
 	public void doStuffForAnHour(int hour){
 		if(hour<=18&&hour>=6){
-			//lmao
+			int a = rand.randInt(4);
+			if (a==0&&x!=500&&y!=500&&Farm.getGrid[x+1][y+1]==null){
+				move(x+1, y+1);
+			}
+			if (a==1&&x!=500&&y!=0&&Farm.getGrid[x+1][y-1]==null){
+				move(x+1, y-1);
+			}
+			if (a==2&&x!=0&&y!=500&&Farm.getGrid[x-1][y+1]==null){
+				move(x-1, y+1);
+			}
+			if (a==3&&x!=0&&y!=0&&Farm.getGrid[x-1][y-1]==null){
+				move(x-1, y-1);
+			}
 		}
+	}
+	public void move(int newX, int newY){
+		Farm.removeObject(this);
+		this.x=newX;
+		this.y=newY;
+		Farm.addObject(this);
 	}
 }
--- FarmObject.java
+++ FarmObject.java
@@ -2,7 +2,18 @@ public abstract class FarmObject{
 	private int x;
 	private int y;
 	public abstract void doStuffForAnHour(int hour);
-	public static void remove(){
+	public void remove(){
 		removeObject(x,y);
+	}
+	public FarmObject(int x, int y){
+		this.x=x;
+		this.y=y;
+		Farm.addObj(this);
+	}
+	public int getX(){
+		return x;
+	}
+	public int getY(){
+		return y;
 	}
 }
